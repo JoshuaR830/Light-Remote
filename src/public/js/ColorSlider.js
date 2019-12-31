4f4ae034dbc5c3b886897936a3fe359bba13d231
Joshua <joshua.richardson@myunidays.com>
2019-12-31 11:26:38 +0000

    Brightness slider change update the lights

--- src/public/js/ColorSlider.js
+++ src/public/js/ColorSlider.js
@@ -2,3 +2,11 @@ function updateSlider(red, green, blue) {
     var root = document.documentElement;
     root.style.setProperty('--color', `rgb(${red}, ${green}, ${blue})`)
 }
+
+window.addEventListener('load', function() {
+    var slider = document.getElementById('brightness-slider');
+    slider.addEventListener('onchange',function(event) {
+        setRGB(red, green, blue);
+    });
+
+}
