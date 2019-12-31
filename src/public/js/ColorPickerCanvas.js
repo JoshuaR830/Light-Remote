5f750f206be43252b5366d4fc7f5926cb067e616
Joshua <joshua.richardson@myunidays.com>
2019-12-31 14:05:45 +0000

    Fixeed some css

--- src/public/js/ColorPickerCanvas.js
+++ src/public/js/ColorPickerCanvas.js
@@ -14,9 +14,42 @@ window.addEventListener('load', function() {
     canvas.classList.add('color-picker-canvas');
 
     canvas.addEventListener('mousedown', function(event) {
+        console.log(event.layerX, event.layerY);
         var imageData = context.getImageData(event.layerX, event.layerY, 1, 1);
+
+        // var rect = canvas.getBoundingClientRect();
+
+        // var xPos = event.clientX - rect.left;
+        // let yPos = event.clientY - rect.top;
+
+
+        // var imageData = context.getImageData(xPos, yPos, 1, 1);
+
         setRGB(imageData.data[0], imageData.data[1], imageData.data[2]);
-    })
+    });
+
+    canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+        }, false);
+
+    function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  console.log("Touch");
+  console.log(touchEvent.touches[0].clientX - rect.left);
+  console.log(touchEvent.touches[0].clientY - rect.top);
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
 
 })
+
+
 
