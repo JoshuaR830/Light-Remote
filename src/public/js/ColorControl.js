645b2535cbead966c0780b151b50e93ffb31ed0d
Joshua <joshua.richardson@myunidays.com>
2019-12-31 11:15:44 +0000

    Getting ready to set brightness

--- src/public/js/ColorControl.js
+++ src/public/js/ColorControl.js
@@ -2,11 +2,16 @@ var currentlyActiveColor = "255000000";
 var red = 0;
 var green = 0;
 var blue = 0;
+var brightness = 0;
 
 function setRGB(r, g, b) {
     red = r.toString().padStart(3, 0);
     green = g.toString().padStart(3, 0);
     blue = b.toString().padStart(3, 0);
+
+    var a = document.getElementById('brightness-slider').value;
+    console.log(`Brightness: ${a}`);
+    brightness = a.toString().padStart(3, 0);
 
     makeColorRequest("pre-defined");
 }
@@ -27,6 +32,7 @@ function submitColour() {
     red = parseInt(document.getElementById('red-manual').value, 10);
     green = parseInt(document.getElementById('green-manual').value, 10);
     blue = parseInt(document.getElementById('blue-manual').value, 10);
+    brightness = parseInt(document.getElementById('brightness-slider').value, 10);
 
     if(red > 255 || red < 0) {
         red = 0;
@@ -40,18 +46,22 @@ function submitColour() {
         blue = 0;
     }
 
+    if(brightness > 255 || brightness < 0) {
+        brightness = 100;
+    }
+
     red = red.toString().padStart(3, 0);
     green = green.toString().padStart(3, 0);
     blue = blue.toString().padStart(3, 0);
+    brightness = brightness.toString().padStart(3, 0);
     
-
     makeColorRequest("user-defined");
 }
 
 function makeColorRequest(color) {
     updateSlider(red, green, blue) 
     var xhttp = new XMLHttpRequest();
-    var parameters = `red=${red}&green=${green}&blue=${blue}`;
+    var parameters = `red=${red}&green=${green}&blue=${blue}&brightness=${brightness}`;
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             if (this.responseText === "success") {
