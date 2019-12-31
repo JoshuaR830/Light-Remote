53f0b005a120bc0581281065dc964111157c119e
Joshua <joshua.richardson@myunidays.com>
2019-12-31 17:50:04 +0000

    Fixed duplication

--- src/public/js/ColorPickerCanvas.js
+++ src/public/js/ColorPickerCanvas.js
@@ -27,28 +27,6 @@ window.addEventListener('load', function() {
 
         setRGB(imageData.data[0], imageData.data[1], imageData.data[2]);
     });
-
-    canvas.addEventListener("touchstart", function (e) {
-        mousePos = getTouchPos(canvas, e);
-        var touch = e.touches[0];
-        var mouseEvent = new MouseEvent("mousedown", {
-            clientX: touch.clientX,
-            clientY: touch.clientY
-        });
-        canvas.dispatchEvent(mouseEvent);
-        }, false);
-
-    function getTouchPos(canvasDom, touchEvent) {
-  var rect = canvasDom.getBoundingClientRect();
-  console.log("Touch");
-  console.log(touchEvent.touches[0].clientX - rect.left);
-  console.log(touchEvent.touches[0].clientY - rect.top);
-  return {
-    x: touchEvent.touches[0].clientX - rect.left,
-    y: touchEvent.touches[0].clientY - rect.top
-  };
-}
-
 })
 
 
