4c530f76733d7eaaa4398314d9483c32016a0935
Joshua <joshua.richardson@myunidays.com>
2020-01-01 15:12:10 +0000

    Scores have been added in a basic sense

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-
 var names = [];
+var scores = {};
 
-var categories = ["christmas", "sport"]
-var charades = [["Christmas tree", "Snowball", "Tin of roses"], ["Footballer", "Tennis player", "Snooker player"]]
+var categories = ["christmas", "sport", "france", "technology", "animals", "books", "countries", "politics"]
+var initialCharades = [
+    ["Christmas tree", "Snowball", "Tin of roses"], 
+    ["Footballer", "Tennis player", "Snooker player"], 
+    ["Baguette", "Eiffel Tower", "Croissant", "Louvre", "Mont St Michel", "Arc du triomphe", "Moulin Rouge", "Beret", "Salted butter", "Breton top (stripey long-sleeved tshirt)", "Champagne", "the French flag"],
+    ["Computer", "Phone", "Robot", "Computer mouse", "Computer keyboard", "Headphones", "MP3 player", "Facebook", "Texting", "Camera", "TV", "Kindle", "App (application)"],
+    ["Emu", "ostrich", "ant eater", "armadillo", "giraffe", "dolphin", "turtle", "elephant", "Lion", "koala", "kangaroo", "hedgehog", "piranha", "jelly fish", "sea urchin", "jelly fish", "squid", "Penguin", "otter", "narwhal", "unicorn", "bear", "lemur", "highland cow", "hardy sheep", "centipede", "dragon", "marmot", "meerkat", "sloth", "panda"],
+    ["Pride and Prejudice", "The Famous Five", "James and the Giant Peach", "The Bible", "Sherlock Holmes", "Little Red Riding Hood", "Peter Rabbit", "Little Women", "To kill a mockingbird", "Harry Potter", "1984", "Animal Farm", "The Cat in the Hat", "Romeo and Juliet", "Of Mice and Men", "Alice in Wonderland", "The Lion, the Witch and the Wardrobe"],
+    ["Finland", "France", "Germany", "North Korea", "China", "Mexico", "Antarctica", "Morocco", "Japan", "Australia", "The Neverlands", "Spain", "Switzerland", "Zimbabwe"],
+    ["Brexit", "voting", "Boris Johnson", "Jeremy Corbyn", "Michael Gove", "Jacob Reese Mogg", "the houses of parliament", "Conservatives", "Labour", "Liberal Democrats", "SNP"]
+]
+
+var charades = initialCharades;
 
 var answer;
 
@@ -20,12 +31,14 @@ function selectCharade() {
         var numCategories = charades.length;
 
         if(numCategories === 0) {
-            charades = [["Christmas tree", "Snowball", "Tin of roses"], ["Footballer", "Tennis player", "Snooker player"]]
+            charades = initialCharades;
             numCategories = charades.length;
         }
 
         var categoryToSelect = (Math.floor(Math.random() * 10) % numCategories);
         var numCharades = charades[categoryToSelect].length;
+
+
         if(numCharades === 0){
             charades.splice(categoryToSelect, 1);
         }
@@ -52,22 +65,34 @@ io.on('connection', function(socket) {
 
     socket.on('new-user-name', function(name) {
         console.log(name);
-        names[names.length] = name;
-        console.log(names);
+        if(!(name in scores)) {
+            names[names.length] = name;
+            scores[name] = 0;
+        }
+        console.log(scores);
+        socket.emit('load-score-data', scores, names);
+
     });
+
+    socket.on('increment-score', function(name) {
+        console.log(`${name} needs their score incremented`);
+        scores[name] += 1;
+        socket.emit('load-score-data', scores, names);
+    })
 
     socket.on('user-revealed-answer', function() {
         console.log(`answer ${answer}`);
         socket.broadcast.emit('reveal-answer', answer);
     });
 
-    socket.on('user-selected-new-card', function() {
+    socket.on('user-selected-new-card', function(name) {
+
         console.log("New");
         response = selectCharade();
         console.log(answer);
         socket.emit('my-charade', response);
         socket.broadcast.emit('set-colour', response[1]);
-        socket.broadcast.emit('new-card');
+        socket.broadcast.emit('new-card', name);
     })
 });
 
--- src/public/charades/js/charade-socket.js
+++ src/public/charades/js/charade-socket.js
@@ -10,31 +10,62 @@ socket.on('reveal-answer', function(answer) {
     dissapear();
     setTimeout(appear, 500);
     document.getElementById('new-charade').style.display = 'inline-block';
-    document.getElementById('reveal-button').style.display = 'inline-block';
 });
 
 socket.on('my-charade', function(charade) {
     yourCharade = charade[0];
     category = charade[1];
     console.log(`Category: ${category}, Charade: ${charade}` );
-    setLightColour()
+    setLightColour();
     setColours();
     dissapear();
     setTimeout(appear, 500);
 });
 
 function changeContent() {
-    var card = this.document.getElementById('charade-card');
+    var card = document.getElementById('charade-card');
     var title = card.querySelector('.title');
     var body = card.querySelector('.body');
 
-    title.innerText = yourCharade;
-    body.innerText = `Act like a ${yourCharade}`
+    title.innerText = toSentenceCase(yourCharade);
+    body.innerText = `Act like ${yourCharade}`;
 }
 
-socket.on('new-card', function() {
+function displayPlayers(scores, players) {
+    var playerContainer = document.getElementById('player-container');
+    playerContainer.innerHTML = "";
+    console.log("Hello");
+    for(i = 0; i < players.length; i ++) {
+        score = scores[players[i]];
+        let currentPlayer = players[i]
+        var div = document.createElement('div');
+        div.classList.add("button");
+        console.log(currentPlayer);
+        div.onclick = function () {
+            console.log(currentPlayer);
+            incrementScore(currentPlayer);
+        }
+        div.innerText = `${players[i]} = ${score}`;
+        playerContainer.appendChild(div);
+    }
+
+}
+
+function incrementScore(name) {
+    socket.emit("increment-score", name);
+}
+
+socket.on('new-card', function(name) {
+    var card = document.getElementById('charade-card');
+
     document.getElementById('new-charade').style.display = 'none';
     document.getElementById('reveal-button').style.display = 'none';
+
+    var title = card.querySelector('.title');
+    var body = card.querySelector('.body');
+
+    title.innerText = `It's ${toSentenceCase(name)}'s turn`;
+    body.innerText = `Start guessing what ${name} is acting`;
 })
 
 window.addEventListener('load', function() {
@@ -42,45 +73,87 @@ window.addEventListener('load', function() {
     charades = document.getElementById('charades');
 });
 
-function hideLogin() {
+socket.on('load-score-data', function(scores, names) {
+    console.log(scores);
+    displayPlayers(scores, names);
+});
+
+function hideLogin(name) {
     login.style.display = 'none';
     charades.style.display = 'inline-block';
+    document.getElementById("my-user").value = name;
 }
 
 function submitUserName() {
     var name = document.getElementById('name-input').value;
     socket.emit('new-user-name', name);
-    hideLogin();
+    hideLogin(name);
 }
 
 function revealAnswer() {
     console.log("Revealed");
     socket.emit('user-revealed-answer');
     document.getElementById('new-charade').style.display = 'inline-block';
+    document.getElementById('reveal-button').style.display = 'none';
 }
 
 function newCard() {
     console.log('new-card-selected');
-    socket.emit('user-selected-new-card');
+    var name = document.getElementById('my-user').value;
+    socket.emit('user-selected-new-card', name);
     document.getElementById('new-charade').style.display = 'none';
+    document.getElementById('reveal-button').style.display = 'inline-block';
+}
+
+function toSentenceCase(text) {
+    return `${text.charAt(0).toUpperCase()}${text.substring(1)}`;
 }
 
 function setColours() {
     var root = document.documentElement;
+
+    document.getElementById("category").innerText = toSentenceCase(category);
 
     if(category === "christmas") {
         root.style.setProperty("--card-color", "rgb(181, 5, 17)");
         root.style.setProperty("--accent-color", "rgb(209, 19, 32)");
         root.style.setProperty("--border-color", "rgb(107, 20, 26)");
         root.style.setProperty("--text-color", "rgb(196, 147, 150)");
-        // changeLightColour(181, 5, 17);
     } else if (category === "sport") {
         root.style.setProperty("--card-color", "rgb(18, 102, 219)");
         root.style.setProperty("--accent-color", "rgb(87, 126, 181)");
         root.style.setProperty("--border-color", "rgb(62, 86, 120)");
         root.style.setProperty("--text-color", "rgb(11, 43, 89)");
-        // changeLightColour(18, 102, 219);
-
+    } else if (category === "france") {
+        root.style.setProperty("--card-color", "rgb(0, 85, 164)");
+        root.style.setProperty("--accent-color", "rgb(239, 65, 53)");
+        root.style.setProperty("--border-color", "rgb(239, 65, 53)");
+        root.style.setProperty("--text-color", "rgb(0, 0, 0)");
+    } else if (category === "technology") {
+        root.style.setProperty("--card-color", "rgb(95, 24, 128)");
+        root.style.setProperty("--accent-color", "rgb(143, 40, 191)");
+        root.style.setProperty("--border-color", "rgb(110, 55, 135)");
+        root.style.setProperty("--text-color", "rgb(0, 0, 0)");
+    } else if (category === "animals") {
+        root.style.setProperty("--card-color", "rgb(130, 87, 8)");
+        root.style.setProperty("--accent-color", "rgb(219, 116, 13)");
+        root.style.setProperty("--border-color", "rgb(184, 95, 7)");
+        root.style.setProperty("--text-color", "rgb(217, 155, 93)");
+    } else if (category === "books") {
+        root.style.setProperty("--card-color", "rgb(0, 242, 255)");
+        root.style.setProperty("--accent-color", "rgb(14, 189, 199)");
+        root.style.setProperty("--border-color", "rgb(23, 148, 156)");
+        root.style.setProperty("--text-color", "rgb(20, 72, 156)");
+    } else if (category === "countries") {
+        root.style.setProperty("--card-color", "rgb(31, 135, 47)");
+        root.style.setProperty("--accent-color", "rgb(52, 168, 70)");
+        root.style.setProperty("--border-color", "rgb(81, 189, 98)");
+        root.style.setProperty("--text-color", "rgb(47, 71, 51)");
+    } else if (category === "politics") {
+        root.style.setProperty("--card-color", "rgb(56, 51, 45)");
+        root.style.setProperty("--accent-color", "rgb(97, 93, 88)");
+        root.style.setProperty("--border-color", "rgb(82, 80, 78)");
+        root.style.setProperty("--text-color", "rgb(232, 225, 218)");
     }
 }
 
@@ -89,6 +162,18 @@ function setLightColour() {
         changeLightColour(181, 5, 17);
     } else if (category === "sport") {
         changeLightColour(18, 102, 219);
+    } else if (category === "france") {
+        changeLightColour(0, 85, 164);
+    } else if (category === "technology") {
+        changeLightColour(95, 24, 128);
+    } else if (category === "animals") {
+        changeLightColour(130, 87, 8);
+    } else if (category === "books") {
+        changeLightColour(0, 242, 255);
+    } else if (category === "countries") {
+        changeLightColour(31, 135, 47);
+    } else if (category === "politics") {
+        changeLightColour(56, 51, 45);
     }
 }
 
